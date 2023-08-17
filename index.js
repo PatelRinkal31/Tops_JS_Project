96f17c868883001cf28fa3ec67fcf88d76b713fc
Rinkal <bhimanirinkal31@gmail.com>
2023-08-17 19:33:19 +0530

    Added lecture17 to Lecture21

---
+++ index.js
@@ -0,0 +1,5 @@
+const input = require("promt-syns")();
+const num1 = input('Enter first number: ');
+const num2 = input('Enter second NUmber');
+const add = parseInt(num1) + parseInt(num2);
+console.log(add);
