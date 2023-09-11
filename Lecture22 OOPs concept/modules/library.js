79187af4acbad1e5847a3d398e4ca786b06dca3a
Rinkal <bhimanirinkal31@gmail.com>
2023-09-11 13:33:00 +0530

    Added programs

---
+++ Lecture22 OOPs concept/modules/library.js
@@ -0,0 +1,7 @@
+import {default as yahoo} from './main';
+yahoo();
+
+// export let message = 'Jay Shree Radhe Krushna';
+// export function hello(){
+//     console.log('Hello this is function called here');
+// }
---
+++ Lecture22 OOPs concept/modules/main.js
@@ -0,0 +1,9 @@
+export default function(){
+    console.log('default function');
+}
+
+// import { message } from "./library";
+// import { hello } from "./library";
+// console.log('dskjsdkjdfk')
+// console.log(message);
+// hello();
